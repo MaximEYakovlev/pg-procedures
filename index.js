fa4b09f60a3037c44acad6ec4a7faafc2923e627
Maxim Yakovlev <it.trainee@yandex.ru>
2024-10-13 18:06:44 +0300

    added: procedure

--- index.js
+++ index.js
@@ -14,4 +14,33 @@ const connect = async () => {
     }
 }
 
-connect();
+const createTable = async () => {
+    await sequelize.query(`
+        CREATE TABLE IF NOT EXISTS tbl (id SERIAL NOT NULL PRIMARY KEY, name VARCHAR(10)); 
+    `);
+}
+
+const createProcedure = async () => {
+    await sequelize.query(`
+        CREATE OR REPLACE PROCEDURE insert_data(a VARCHAR(10))
+        LANGUAGE SQL
+        BEGIN ATOMIC
+          INSERT INTO tbl (name) VALUES (a);
+        END;
+    `);
+}
+
+const call = async () => {
+    await sequelize.query(`
+        CALL insert_data('Foo');
+    `);
+}
+
+const run = async () => {
+    await connect();
+    await createTable();
+    await createProcedure();
+    await call();
+}
+
+run();
